1abc0d2eaabef31f0712dcb909b7738a3a92b877
Makoto Emura <makoto@emurasoft.com>
2023-08-17 17:59:12 -0700

    Add files

---
+++ template-files/template.scala
@@ -0,0 +1,14 @@
+﻿/* Name it "HelloWorld.scala" 
+ * Compile it by running "scalac HelloWorld.scala"
+ * Run it by typing "scala HelloWorld.scala"
+ */
+
+object HelloWorld 
+{
+	def main(args: Array[String]) = 
+	{
+		Console.println("Hello, world!");
+	}
+}
+
+
